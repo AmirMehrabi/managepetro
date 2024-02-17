5b06423ab07b58ed86c8c323d8a739bd937c7137
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2024-02-17 12:47:12 +0330

    Working on the general UI

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth')->group(function () {
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+});
+
+Auth::routes();
