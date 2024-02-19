e9b869ea32a80caf0cca6168444467ce2f10e1c1
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2024-02-20 03:16:06 +0330

    Removed an extra route

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
